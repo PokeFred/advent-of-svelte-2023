c7976c20aefa0fd74e98455f3ae2e8e895609662
PokeFred <cedric.paelmke@gmx.de>
2023-12-20 22:26:00 +0100

    solve day two

--- src/routes/day-two/+page.svelte
+++ src/routes/day-two/+page.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
-    import type {PageServerData} from "./$types"
     import LayoutTemplate from "$components/LayoutTemplate.svelte"
 
-    export let data: PageServerData
+    let counter: number = 0;
+
+    function add(): void {
+        counter++
+    }
+
+    function remove(): void {
+        if (counter === 0) return
+        counter--
+    }
+
+    function reset(): void {
+        counter = 0
+    }
 </script>
 
-<LayoutTemplate name="Day 02"></LayoutTemplate>
+<LayoutTemplate name="Day 02">
+    <div class="p-4">
+        <div class="my-2 text-3xl">Cookies: {counter}</div>
+        <div><button on:click={add} class="my-2 w-fit h-auto text-xl bg-green-700 rounded-xl px-4">Add</button></div>
+        <div><button on:click={remove} class="my-2 w-fit h-auto text-xl bg-blue-700 rounded-xl px-4">Remove</button></div>
+        <div><button on:click={reset} class="my-2 w-fit h-auto text-xl bg-red-700 rounded-xl px-4">Reset</button></div>
+    </div>
+</LayoutTemplate>
